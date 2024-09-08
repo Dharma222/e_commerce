e5982770fafd600c0a7e82d600dea4382379e9dd
Dharma teja <dharmatejabomisety222@gmail.com>
2024-09-08 23:22:14 +0530

    Inventory service was implemented

--- src/main/java/dev/bhargav/wipro/Controller/InventoryController.java
+++ src/main/java/dev/bhargav/wipro/Controller/InventoryController.java
@@ -1,4 +1,47 @@
 package dev.bhargav.wipro.Controller;
 
+import dev.bhargav.wipro.Dtos.CustomerRequestdto;
+import dev.bhargav.wipro.Dtos.InventoryRequestdto;
+import dev.bhargav.wipro.Service.InventoryService;
+import dev.bhargav.wipro.models.Customer;
+import dev.bhargav.wipro.models.Inventory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class InventoryController {
+    public InventoryService inventoryService;
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @PostMapping("/api/inventory")
+    public Inventory updateInventory(@RequestBody InventoryRequestdto inventoryRequestdto) {
+        return inventoryService.addinventory(
+                inventoryRequestdto.getId(),
+                inventoryRequestdto.getProduct_id(),
+                inventoryRequestdto.getQuantity());
+
+
+    }
+
+    @DeleteMapping("/api/inventory/{id}")
+    public Inventory deleteinventory(@PathVariable("id") int Id)
+    {
+        return inventoryService.deleteinventory(Id);
+    }
+
+    @PutMapping("/api/updateinventory/{id}")
+    public Inventory updateinventory(@PathVariable("id") int Id,@RequestBody InventoryRequestdto inventoryRequestdto)
+    {
+        return inventoryService.updateinventory(Id,
+                inventoryRequestdto.getId(),
+                inventoryRequestdto.getProduct_id(),
+        inventoryRequestdto.getQuantity());
+    }
+
+    @GetMapping("/api/searchinventory/{id}")
+    public Inventory searchcustomer(@PathVariable("id") int Id)
+    {
+        return inventoryService.getinventory(Id);
+    }
 }
--- src/main/java/dev/bhargav/wipro/Service/InventoryService.java
+++ src/main/java/dev/bhargav/wipro/Service/InventoryService.java
@@ -1,4 +1,41 @@
 package dev.bhargav.wipro.Service;
 
+
+import dev.bhargav.wipro.Repository.InventoryRepository;
+import dev.bhargav.wipro.models.Customer;
+import dev.bhargav.wipro.models.Inventory;
+import org.springframework.data.relational.core.sql.In;
+import org.springframework.stereotype.Service;
+
+@Service
 public class InventoryService {
+
+    public InventoryRepository inventoryRepository;
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+    public Inventory addinventory(int id, int productid , int qunatity )
+    {
+        Inventory inventory = new Inventory();
+        inventory.setInventory_id(id);
+        inventory.setProduct_id(productid);
+        inventory.setQuantity(qunatity);
+        return inventoryRepository.save(inventory);
+    }
+
+    public Inventory deleteinventory(int id) {
+        return inventoryRepository.deleteById(id);
+    }
+
+    public Inventory getinventory(int id) {
+        return inventoryRepository.findById(id);
+    }
+
+    public Inventory updateinventory(int id,int inventoryid, int productid, int qunatity) {
+        Inventory inventory = getinventory(id);
+        inventory.setInventory_id(inventoryid);
+        inventory.setProduct_id(productid);
+        inventory.setQuantity(qunatity);
+        return inventoryRepository.save(inventory);
+    }
 }
